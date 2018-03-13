f3862a5b02346686c5c20570ed8f77158dca6359
michael hohl <mhohl@rocketmail.com>
2018-03-13 20:23:50 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,1 +1,4 @@
 # Write your #display_rainbow method here
+def display_rainbow (array)
+  puts "R: #{array[0]}, O: #{array[0]}, Y: #{array[0]}, G: #{array[0]}, B: #{array[0]}, I: #{array[0]}, V: #{array[0]}"
+end
